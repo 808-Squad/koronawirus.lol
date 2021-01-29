d7362a26642594da63e8bc1097b26b17acf974c1
Kamil Dzierzanowski <Kamil.Dzierzanowski@vimn.com>
2021-01-29 17:03:40 +0100

    Join entries for 1-4, 9-12, 13-17 and 27-28

--- src/generation-logic/generate.js
+++ src/generation-logic/generate.js
@@ -11,12 +11,50 @@ xhr.onload = () => {
     if (xhr.readyState === 4) {
         if (xhr.status === 200) {
             data = xhr.responseText.replace(/[\r]+/g, '').split('\n').filter(e => e);
+            for (let i = 0; i < data.length; i++) {
+                data[i] = data[i].replace("\"", "").split(',').filter(e => e);
+            }
         } else {
             console.error(xhr.statusText);
         }
     }
 };
 xhr.send(null);
+
+// Durstenfeld Shuffle from https://stackoverflow.com/a/12646864/6417161
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function getRandomRange(first, last, data) {
+    last += 1;  // Last index in slices etc. is usually 1 higher than what you need
+    const delta = last - first;
+
+    let arr = [];
+    for (let i = first; i < last; i++) {
+        arr = arr.concat(data[i]);
+    }
+    const len = arr.length;
+    shuffleArray(arr);
+
+    const result = [];
+    for (let i = 0; i < delta; i++) {
+        const idx = Math.floor(Math.random() * (len - 1));
+        result.push(arr.splice(idx, 1));
+    }
+    return result;
+}
+
+function getRandom(n, data) {
+    const arr = data[n];
+    const len = arr.length;
+    shuffleArray(arr);
+    const idx = Math.floor(Math.random() * (len - 1));
+    return arr[idx];
+}
 
 function quarantine_decision() {
     let rnd = Math.random();
@@ -35,24 +73,28 @@ function bus_seats(percentage) {
 }
 
 export default function generate() {
-    let arr = data.map(arr => {
-        arr = arr.split(',').filter(e => e);
-        console.log(arr);
-        const len = arr.length;
-        let rnd_index = Math.floor(Math.random() * len);
-        return arr[rnd_index].replace("\"", "");
-    });
+    let arr = [];
+    [arr[0], arr[1], arr[2], arr[3]] = getRandomRange(0, 3, data);
+    arr[4] = getRandom(4, data);
+    arr[5] = getRandom(5, data);
     arr[6] = quarantine_decision();
+    arr[7] = getRandom(7, data);
+    [arr[8], arr[9], arr[10], arr[11]] = getRandomRange(8, 11, data);
+    [arr[12], arr[13], arr[14], arr[15], arr[16]] = getRandomRange(12, 16, data);
+    arr[17] = getRandom(17, data);
+    arr[18] = getRandom(18, data);
+    arr[19] = getRandom(19, data);
+    arr[20] = getRandom(20, data);
+    arr[21] = getRandom(21, data);
+    arr[22] = getRandom(22, data);
+    arr[23] = getRandom(23, data);
+    arr[24] = getRandom(24, data);
+    arr[25] = getRandom(25, data);
+    [arr[26], arr[27]] = getRandomRange(26, 27, data);
+    arr[28] = getRandom(28, data);
+    arr[29] = getRandom(29, data);
+    arr[30] = getRandom(30, data);
+    arr[31] = getRandom(31, data);
     arr[32] = bus_seats(arr[31]);
     return arr;
-    // return [
-    //     'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    //     'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
-    //     'Contrary to popular belief, Lorem Ipsum is not simply random text.',
-    //     'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
-    //     'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
-    //     'Contrary to popular belief, Lorem Ipsum is not simply random text.',
-    //     'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
-    //     'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form'
-    // ]
 }
