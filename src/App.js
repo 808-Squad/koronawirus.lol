66ae2e1cfd9660e910606f808188eaf657f3e2a6
Waked <waked@incens.io>
2021-01-31 04:24:43 +0100

    Add GitHub link in footer

--- src/App.js
+++ src/App.js
@@ -233,14 +233,54 @@ const Footer = styled.div`
   background-color: white;
   padding-left: 1rem;
   padding-right: 1rem;
-  font-size: 10px;
-  font-style: italic;
   border-top: solid rgb(213, 35, 63) .1875rem;
   clear: both;
-  text-align: center;
   width: 100vw;
   flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
+
+const FooterText = styled.footer`
+  width: 95vw;
+  font-size: 11px;
+  font-style: italic;
+  @media (min-width: ${breakpoints.md}px) {
+    font-size: 11px;
+    width: 600px;
+  }
+  @media (min-width: ${breakpoints.lg}px) {
+    width: 750px;
+  }
+  @media (min-width: ${breakpoints.xxl}px) {
+    width: 900px;
+    font-size: 14px;
+  }
+  @media (min-width: ${breakpoints.xxxl}px) {
+    width: 1400px;
+    font-size: 20px;
+  }
+`
+
+const GithubIcon = styled.span`
+  display: inline-block;
+  width: 15px;
+  height: 1em;
+  background-image: url('github.svg');
+  background-position: center;
+  background-repeat: no-repeat;
+`
+
+const LikeIcon = styled.span`
+  display: inline-block;
+  width: 15px;
+  height: 1em;
+  background-image: url('like.svg');
+  background-position: center;
+  background-repeat: no-repeat;
+`
+
 
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -343,8 +383,13 @@ function App(props) {
           <StyledButton onClick={buttonAction}>{buttonText}</StyledButton>
         </SentencesContainer>
         <Footer>
-          Ta strona to <b>żart</b>. Po prawdziwe informacje na temat obostrzeń udaj się <a href="https://www.gov.pl/web/koronawirus">tutaj</a>.
-          Ikony dzięki <a href="https://www.freepik.com" title="Freepik">Freepik</a> przez <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+          <FooterText>
+            <b>Ta strona to żart</b>. Po prawdziwe informacje na temat obostrzeń udaj się <a href="https://www.gov.pl/web/koronawirus">tutaj</a>.
+            <br />
+            Zaserduszkuj <LikeIcon/> to na <GithubIcon/> <a href="https://github.com/krethh/koronawirus.lol">GitHubie</a>.
+            <br />
+            Ikony dzięki <a href="https://www.freepik.com" title="Freepik">Freepik</a> przez <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+          </FooterText>
         </Footer>
       </Container>
   );
