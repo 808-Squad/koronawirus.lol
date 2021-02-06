677e4fffa700efbfa70a8cfb2ae8cdef5d47376a
krethh <krecik.kret@gmail.com>
2021-02-06 08:36:45 +0100

    Add internationalization and update README

--- src/index.jsx
+++ src/index.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './ui/App';
-import reportWebVitals from './reportWebVitals';
 import ReactBreakpoints from 'react-breakpoints';
 import breakpoints from './ui/breakpoints.js';
 import { IntlProvider } from 'react-intl';
-import plMessages from './ui/messages/pl.json';
+import { chooseLanguage } from './utils'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <IntlProvider messages={plMessages} locale="pl" defaultLocale="pl">
-      <ReactBreakpoints breakpoints={breakpoints}>
-        <App />
-      </ReactBreakpoints>
-    </IntlProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const messagesPath = `messages/${chooseLanguage()}.json`;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+fetch(messagesPath)
+.then(response => response.json())
+.then(response => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <IntlProvider messages={response} locale="pl" defaultLocale="pl">
+        <ReactBreakpoints breakpoints={breakpoints}>
+          <App />
+        </ReactBreakpoints>
+      </IntlProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+})
