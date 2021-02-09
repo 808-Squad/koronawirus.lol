0f5aa4804b395bd0efb04ca0feb9c56eecd1df81
krethh <krecik.kret@gmail.com>
2021-02-09 17:17:33 +0100

    Remove unnecessary store

--- src/index.jsx
+++ src/index.jsx
@@ -5,14 +5,10 @@ import App from './ui/App';
 import ReactBreakpoints from 'react-breakpoints';
 import breakpoints from './ui/breakpoints.js';
 import { IntlProvider } from 'react-intl';
-import { store } from './store/store'
-import { setLanguage } from './store/reducers/languageReducer';
 import { chooseLanguage } from './utils'
 
 const language = chooseLanguage()
 const messagesPath = `messages/${language}.json`;
-
-store.dispatch(setLanguage(language))
 
 fetch(messagesPath)
 .then(response => response.json())
